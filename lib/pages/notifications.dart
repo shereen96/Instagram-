6be22585e97e8ef178ae38192f0635f262b65aa2
Shereen Al-shear <shereen1996-m@hotmail.com>
2020-08-30 05:06:17 +0200

    Add files via upload

---
+++ lib/pages/notifications.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/models/post.dart';
+import 'package:instagram/models/user.dart';
+import 'package:instagram/models/comment.dart';
+import 'package:instagram/models/global.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
